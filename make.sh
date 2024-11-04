297f5c74655a4a95c5dac575849710e57348e19d
Artem V. Ageev <artem.v.ageev@gmail.com>
2024-11-04 18:07:47 +0200

    fix ci

--- make.sh
+++ make.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+##############################################################################################################
 
 function priv_clippit
 (
@@ -9,32 +10,34 @@ Options:
 EOF
 )
 
-function pub_build
-(
-    git submodule update --init --recursive
-    find 'use' -type 'f' -name '*.lpk' -exec lazbuild --add-package-link {} \;
-    find 'src' -type 'f' -name '*.lpi' -exec lazbuild --recursive --build-mode=release {} \;
-)
-
 function priv_main
 (
     set -euo pipefail
-    if ! (which lazbuild); then
-        source '/etc/os-release'
-        case ${ID:?} in
-            debian | ubuntu)
-                sudo apt-get update
-                sudo apt-get install -y lazarus
-            ;;
-        esac
-    fi
     if ((${#})); then
         case ${1} in
-            build) pub_build 1>&2 ;;
+            build)
+                if ! (which lazbuild); then
+                    source '/etc/os-release'
+                    case ${ID:?} in
+                        debian | ubuntu)
+                            sudo apt-get update
+                            sudo apt-get install -y lazarus
+                        ;;
+                    esac
+                fi
+                if [[ -d 'use' ]]; then
+                    git submodule update --recursive --init
+                    git submodule update --recursive --remote
+                    find 'use' -type 'f' -name '*.lpk' -exec lazbuild --add-package-link {} +
+                fi
+                find 'src' -type 'f' -name '*.lpi' \
+                    -exec lazbuild --no-write-project --recursive --no-write-project --build-mode=release {} + 1>&2
+                ;;
         esac
     else
         priv_clippit
     fi
 )
 
+##############################################################################################################
 priv_main "${@}" >/dev/null
